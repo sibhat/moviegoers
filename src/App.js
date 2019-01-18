41143ba9450daf2dbc9ddb08fde9138588a78c74
sibhat <sibhat.temelso@gmail.com>
2019-01-17 17:38:45 -0800

    folder setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
 	componentDidMount() {
 		axios
 			.get(
-				"https://api.themoviedb.org/3/movie/297802/videos?api_key=4d9dd02575caf8f6d545ff421762869e&language=en-US"
+				"https://api.themoviedb.org/3/movie/297802/videos?api_key=" +
+					process.env.MOVIEDB_API +
+					"e&language=en-US"
 			)
 			.then(result => {
 				console.log({ result });
