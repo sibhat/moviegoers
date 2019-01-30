3a6505fb319b9820e33a03a06a02e15306d84703
sibhat <sibhat.temelso@gmail.com>
2019-01-29 23:39:00 -0800

    update test case

--- src/feature/detail/store/storeTest.test.js
+++ src/feature/detail/store/storeTest.test.js
@@ -34,7 +34,7 @@ let detailMovieMockReturnResult = {
 	original_title: "दिलवाले दुल्हनिया ले जायेंगे",
 	overview:
 		"Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.",
-	popularity: 17.549,
+	popularity: 17.133,
 	poster_path: "/uC6TTUhPpQCmgldGyYveKRAu8JN.jpg",
 	production_companies: [
 		{
